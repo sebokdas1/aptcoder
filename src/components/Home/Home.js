70b0afb9ee77cea0c9c59937b7279d14e7bbb379
sebokdas1 <sebok.das.ff@gmail.com>
2022-06-17 19:52:46 +0600

    make spinner

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Spinner from '../Loading/Spinner';
+
+const Home = () => {
+    return (
+        <div>
+            <Spinner></Spinner>
+        </div>
+    );
+};
+
+export default Home;
