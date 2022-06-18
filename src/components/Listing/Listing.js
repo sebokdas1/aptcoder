44f0679dffc6447e423fa2f7222358a4779c831e
sebokdas1 <sebok.das.ff@gmail.com>
2022-06-18 11:58:04 +0600

    table

--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import './Listing.css'
 
 const Listing = () => {
+    const [employers, setEmployers] = useState([])
+    useEffect(() => {
+        fetch('employer.json')
+            .then(res => res.json())
+            .then(data => setEmployers(data))
+    }, [])
     return (
         <div>
-
+            <form action="">
+                <input type="text" name="name" id="" />
+                <input type="submit" value="Search" />
+            </form>
+            <div className='table-div'>
+                <table>
+                    <tr>
+                        <th>Name</th>
+                        <th>Post</th>
+                        <th>Age</th>
+                        <th>City</th>
+                    </tr>
+                    {
+                        employers.map(employe => <tr key={employe?.id}>
+                            <td>{employe?.name}</td>
+                            <td>{employe?.post}</td>
+                            <td>{employe?.age}</td>
+                            <td>{employe?.city}</td>
+                        </tr>)
+                    }
+                </table>
+            </div>
         </div>
     );
 };
