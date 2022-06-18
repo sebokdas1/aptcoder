21ddb5cf6ef450e68abd9bbcae9ba1d8a9ba7e42
sebokdas1 <sebok.das.ff@gmail.com>
2022-06-18 15:13:12 +0600

    latest

--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -9,12 +9,24 @@ const Listing = () => {
             .then(res => res.json())
             .then(data => setEmployers(data))
     }, [])
+    const handleSearch = (e) => {
+        e.preventDefault()
+        const name = (e.target.name.value).lowercase();
+        console.log(name)
+        setEmployers(employers?.filter(a => a?.name === name))
+    }
     return (
-        <div>
-            <form action="">
-                <input type="text" name="name" id="" />
-                <input type="submit" value="Search" />
-            </form>
+        <div className='listing-main'>
+            <div className='form-container'>
+                <form onSubmit={handleSearch}>
+                    <input id='nameField' type="text" name="name" placeholder='search by name' required />
+                    <input id='submit' type="submit" value="Search" />
+                </form>
+            </div>
+
+            <div>
+                <h2>Filter By: <button>FrontEnd Developer</button> <button>BackEnd Developer</button> <button>FullStack</button></h2>
+            </div>
             <div className='table-div'>
                 <table>
                     <tr>
@@ -24,7 +36,7 @@ const Listing = () => {
                         <th>City</th>
                     </tr>
                     {
-                        employers.map(employe => <tr key={employe?.id}>
+                        employers?.map(employe => <tr key={employe?.id}>
                             <td>{employe?.name}</td>
                             <td>{employe?.post}</td>
                             <td>{employe?.age}</td>
